5e3c23784dd18b26576d8f5d44698d617818df22
Zach Howarth <zachhowarth@gmail.com>
2019-06-05 09:28:59 -0500

    Complete Iteration 0

---
+++ todo/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  todo
+//
+//  Created by Zachary Howarth on 6/5/19.
+//  Copyright © 2019 Zachary Howarth. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    
+    var important = false
+}
---
+++ todo/ToDoTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ToDoTableViewController.swift
+//  todo
+//
+//  Created by Zachary Howarth on 6/5/19.
+//  Copyright © 2019 Zachary Howarth. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var toDos : [ToDo] = []
+    
+    func createToDos() -> [ToDo] {
+        
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        // important is set to false by default
+        
+        return [swift, dog]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        toDos = createToDos()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
